904c516926a96272bd0358b1fea1c261eb03d362
sainthokaha5@gmail.com <88292276+Sainthokaha@users.noreply.github.com>
2023-08-15 12:56:14 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
-      <div class="search-box"></div>
-      <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather">
+      <div class="search-box">
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
 
-      <div class="weather-wrap" v-if="(typeof weather.main != 'undefined')">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">{{weather.name}}</div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
           <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">{{math.round(weather.main.temp)}}C</div>
-          <div class="weather">{{weather.weather[0].main}}</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -20,40 +27,39 @@
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data() {
+  name: 'app',
+  data () {
     return {
       api_key: '1d2621f6892f2a15d929f5f5de944b0e',
-      url_base: 'https://home.openweathermap.org/',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
     }
   },
   methods: {
-    fetchWeather(e) {
-      if(e.key == 'Enter') {
+    fetchWeather (e) {
+      if (e.key == "Enter") {
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
-        .then(res => {
-          return res.json();
-        }).then(this.setResults)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
       }
     },
-    setResults(results) {
+    setResults (results) {
       this.weather = results;
     },
-    dateBuilder() {
+    dateBuilder () {
       let d = new Date();
-      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-      let day = days[d.get()];
+      let day = days[d.getDay()];
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
 
-      return `${day} ${date} ${month} ${year}`
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
@@ -61,20 +67,24 @@ export default {
 
 <style>
 * {
-  margin: 0; 
-  padding: 0; 
-  box-sizing: border-box; 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 body {
-  font-family: 'montserrat', sans-serif;
+  font-family: 'montserrat', sans-serif;  
+  background: rgb(2,0,36);
+  background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(54,9,121,1) 35%, rgba(0,212,255,1) 100%);
 }
 
 #app {
   background-image: url('./assets/cold-bg.jpg');
   background-size: cover;
   background-position: bottom;
-  transition: .4s;
+  transition: 0.4s;
+  width: 478px;
+  margin: 0 auto;
 }
 
 #app.warm {
@@ -84,7 +94,8 @@ body {
 main {
   min-height: 100vh;
   padding: 25px;
-  background-image: linear-gradient(to bottom, rgba(0, 0, 0, .25), rgba(0, 0, 0, .75));
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
 
 .search-box {
@@ -92,38 +103,41 @@ main {
   margin-bottom: 30px;
 }
 
-.search-bar {
+.search-box .search-bar {
   display: block;
   width: 100%;
   padding: 15px;
+  
   color: #313131;
   font-size: 20px;
+
   appearance: none;
-  border: none;
+  border:none;
   outline: none;
   background: none;
-  box-shadow: 0px 0px 8px rgba(0, 0, 0, .25);
-  background-color: rgba(255, 255, 255, .5);
+
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
   border-radius: 0px 16px 0px 16px;
-  transition: .4s;
+  transition: 0.4s;
 }
 
-.search-box, .search-bar:focus {
-  box-shadow: 0px 0px 16px rgba(0, 0, 0, .25);
-  background-color: rgba(255, 255, 255, .75);
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
 
 .location-box .location {
-  color: #fff;
+  color: #FFF;
   font-size: 32px;
   font-weight: 500;
   text-align: center;
-  text-shadow: 1px 3px rgba(0, 0, 0, .25);
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
 }
 
 .location-box .date {
-  color: #fff;
+  color: #FFF;
   font-size: 20px;
   font-weight: 300;
   font-style: italic;
@@ -137,21 +151,23 @@ main {
 .weather-box .temp {
   display: inline-block;
   padding: 10px 25px;
-  color: #fff;
+  color: #FFF;
   font-size: 102px;
   font-weight: 900;
-  text-shadow: 3px 6px rgba(0, 0, 0, .25);
-  background-color: rgba(255, 255, 255, .25);
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0, 0, 0, .25);
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 
 .weather-box .weather {
-  color: #fff;
+  color: #FFF;
   font-size: 48px;
   font-weight: 700;
   font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
